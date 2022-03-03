acd45f73a59c46c75a2ad137e1c5c8e18ed2ef44
InezMallandain <inezvmallandain@gmail.com>
2022-03-03 11:15:33 +0200

    update css

--- src/components/search-bar.component.vue
+++ src/components/search-bar.component.vue
@@ -22,19 +22,17 @@
 export default {
   data() {
     return {
-      searchText: ""
+      searchText: "",
     };
   },
   methods: {
     onSearch() {
-      this.$emit('search', this.searchText);
-    }
-  }
+      this.$emit("search", this.searchText);
+    },
+  },
 };
 </script>
-<style scoped lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Poppins:weight@100;200;300;400;500;600;700;800&display=swap");
-
+<style scoped>
 .height {
   height: 100%;
 }
